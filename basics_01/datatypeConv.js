d2a5719d90a34e13ffcaa86684eb7cde9240595e
Samrat Sarkar <samratsarkarofficial@gmail.com>
2024-09-05 20:22:30 +0000

    conversion of datatypes

---
+++ basics_01/datatypeConv.js
@@ -0,0 +1,17 @@
+let score = "Samrat"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinScore = Number(score)
+console.log(valueinScore);
+
+
+//NaN --> Not a number
+
+//"33"--> Number
+//"33ab" --> NaN
+// true --> 1
+//null --> 0
+//undefined --> NaN
+//"Samrat" --> NaN
